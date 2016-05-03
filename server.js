e17eb02ca7a23ebbcd4eb5eb640a8ab5294bdf75
Kevin Wade <kevin@kevinwade.name>
2016-05-02 21:06:23 -0500

    Support etag caching

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const { BUCKET, NODE_ENV } = process.env;
 const app = express();
 app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'));
 
+const getHeaders = (req, keys) => {
+  let headers = {};
+  keys.forEach(key => {
+    const val = req.get(key);
+    if (val) headers[key] = val;
+  });
+  return headers;
+};
+
 app.get('*', (req, res) => {
   const { hostname, path } = req;
   const bucketPath = `http://s3.amazonaws.com/${BUCKET}/${hostname}${path}`;
-  request(bucketPath).pipe(res);
+  const headers = getHeaders(req, ['If-None-Match']);
+  request({ url: bucketPath, headers }).pipe(res);
 });
 
 const PORT = process.env.PORT || 5000;
